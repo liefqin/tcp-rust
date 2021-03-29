a4143c6eb23ed7c6fe92ccb8a1f321d9c9074b79
zhushq <409548575@qq.com>
2021-03-30 00:04:22 +0800

    sum

--- lesson4/src/sum.rs
+++ lesson4/src/sum.rs
@@ -1,9 +1,9 @@
 pub fn sum(items:&[u32]) -> Option<u32>{
     let mut sum_num:u32 = 0;
     for item in items{
-        if sum_num > 2^32 -1 {
-            return None;
-        }
+        // if sum_num > 2^32 -1 {
+        //     return None;
+        // }
         sum_num = sum_num + item;
     }
     Some(sum_num)
